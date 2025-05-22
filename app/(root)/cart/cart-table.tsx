a02b334756ccf5b811f9e2b9b7c1a01e854a693e
nohahakim <raafatabass@hotmail.com>
2025-05-22 12:57:02 +0300

    fix

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -9,7 +9,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { formatCurrency } from "@/lib/utils";
 
 import {
   Table,
