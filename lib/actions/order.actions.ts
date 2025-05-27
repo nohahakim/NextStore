90515bd7ccf6b4415e84a2a8ff1ee436ff6cd662
nohahakim <raafatabass@hotmail.com>
2025-05-27 21:16:35 +0300

    deleteOrder

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -317,3 +317,38 @@ export async function getOrderSummary() {
     salesData,
   };
 }
+export async function getAllOrders({
+  limit = PAGE_SIZE,
+  page,
+}: {
+  limit?: number;
+  page: number;
+}) {
+  const data = await prisma.order.findMany({
+    orderBy: { createdAt: "desc" },
+    take: limit,
+    skip: (page - 1) * limit,
+    include: { user: { select: { name: true } } },
+  });
+
+  const dataCount = await prisma.order.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
+}
+export async function deleteOrder(id: string) {
+  try {
+    await prisma.order.delete({ where: { id } });
+
+    revalidatePath("/admin/orders");
+
+    return {
+      success: true,
+      message: "Order deleted successfully",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
